426da58a4cbfc0304c508b5e3ab389aa45792e50
hattyot <hattyot@gmail.com>
2020-01-25 17:02:59 +0200

    users can now gain experience

--- cogs/levels.py
+++ cogs/levels.py
@@ -1,4 +1,10 @@
+from time import time
+from random import randint
 from discord.ext import commands
+from modules import database
+
+db = database.Connection()
+xp_cooldown = {}
 
 
 class Levels(commands.Cog):
@@ -6,7 +12,21 @@ class Levels(commands.Cog):
         self.bot = bot
 
     async def process_message(self, ctx):
-        pass
+        if ctx.author.id in xp_cooldown:
+            if round(time()) >= xp_cooldown[ctx.author.id]:
+                del xp_cooldown[ctx.author.id]
+            else:
+                return
+
+        xp_add = randint(15, 25)
+        self.add_xp(ctx, xp_add)
+
+        cooldown_expire = round(time()) + 45
+        xp_cooldown[ctx.author.id] = cooldown_expire
+
+    def add_xp(self, ctx, xp):
+        db.levels.update_one({'guild_id': ctx.guild.id}, {'$inc': {f'users.{ctx.author.id}.xp': xp}})
+        db.get_levels.invalidate(ctx.guild.id, ctx.author.id, 'xp')
 
 
 def setup(bot):
