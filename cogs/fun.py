a08522c39caefde83d0ad17c341807b681be42c7
Hattyot <Hattyot@gmail.com>
2020-05-08 01:15:47 +0300

    gif distort now only outputs the first frame

--- cogs/fun.py
+++ cogs/fun.py
@@ -38,34 +38,42 @@ class Fun(commands.Cog):
             return
 
         if mem:
-            url = str(mem.avatar_url).replace('.webp?size=1024', '.png?size=1024')
+            url = str(mem.avatar_url).replace('.webp', '.png')
 
-        try:
-            response = requests.get(url)
-            _img = BytesIO(response.content)
-            _img.seek(0)
+        response = requests.get(url)
+        _img = BytesIO(response.content)
+        _img.seek(0)
+
+        with wImage() as new_image:
             with wImage(file=_img) as img:
                 if img.size >= (3000, 3000):
                     embed = embed_maker.message(ctx, 'Image exceeds maximum resolution `3000x3000`', colour='red')
                     return await ctx.send(embed=embed)
 
-                img.transform(resize='800x800')
-                for i in range(layers):
-                    img.liquid_rescale(width=int(img.width * 0.5), height=int(img.height * 0.5), delta_x=1)
-                    img.liquid_rescale(width=int(img.width * 1.5), height=int(img.height * 1.5), delta_x=2)
+                def transform_image(img):
+                    img.transform(resize='800x800')
+                    for i in range(layers):
+                        img.liquid_rescale(width=int(img.width * 0.5), height=int(img.height * 0.5), delta_x=1)
+                        img.liquid_rescale(width=int(img.width * 1.5), height=int(img.height * 1.5), delta_x=2)
 
-                img.transform(resize='800x800')
+                    img.transform(resize='800x800')
 
-                magikd_buffer = BytesIO()
-                img.save(magikd_buffer)
-                magikd_buffer.seek(0)
+                if len(img.sequence) > 1:
+                    transform_image(img.sequence[0])
+                    new_image.sequence.append(img.sequence[0])
+                else:
+                    transform_image(img)
+                    new_image.sequence.append(img)
 
-                embed = discord.Embed()
-                embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
-                embed.set_image(url='attachment://distorted.png')
-                return await ctx.send(file=discord.File(fp=magikd_buffer, filename='distorted.png'), embed=embed)
-        except:
-            return
+
+            magikd_buffer = BytesIO()
+            new_image.save(magikd_buffer)
+            magikd_buffer.seek(0)
+
+            embed = discord.Embed()
+            embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
+            embed.set_image(url='attachment://distorted.png')
+            return await ctx.send(file=discord.File(fp=magikd_buffer, filename='distorted.png'), embed=embed)
 
 
 def setup(bot):
