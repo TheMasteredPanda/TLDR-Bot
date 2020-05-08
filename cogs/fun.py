8c53fee2cc3873ee4278907822d735f10c5efde5
Hattyot <Hattyot@gmail.com>
2020-05-08 13:18:55 +0300

    re-enabled distort command, offloaded image processing onto web api on heroku

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,9 +1,8 @@
 import discord
 import re
-import mimetypes
 import requests
+import config
 from io import BytesIO
-from wand.image import Image as wImage
 from modules import command, embed_maker
 from discord.ext import commands
 
@@ -47,24 +46,20 @@ class Fun(commands.Cog):
 
             return discord.File(fp=magikd_buffer, filename=f'distorted.{ext}'), embed
 
-    @commands.command(help='Distort images or peoples profile pictures', usage='distort [image link | @Member] (layers)',
-                      examples=['disort https://i.imgur.com/75Jr3.jpg', 'distort @Hattyot', 'distort Hattyot 3'], clearance='Dev', cls=command.Command)
-    async def distort(self, ctx, source=None, layers=1):
-        # check if source is member
+    @commands.command(help='Distort images or peoples profile pictures', usage='distort [image link | @Member]',
+                      examples=['disort https://i.imgur.com/75Jr3.jpg', 'distort @Hattyot', 'distort Hattyot'], clearance='User', cls=command.Command)
+    async def distort(self, ctx, source=None):
         url = None
         mem = None
 
+        # check if source is member
         if source and ctx.message.mentions:
             mem = ctx.message.mentions[0]
         elif source:
             regex = re.compile(fr'({source.lower()})')
             mem = discord.utils.find(lambda m: re.findall(regex, m.name.lower()) or re.findall(regex, m.display_name.lower()) or m.id == source, ctx.guild.members)
             if mem is None:
-                # Check if source is image link
-                mimetype, encoding = mimetypes.guess_type(source)
-                if mimetype and mimetype.startswith('image'):
-                    # source is image url
-                    url = source
+                url = source
 
         if source is None:
             mem = ctx.author
@@ -73,41 +68,20 @@ class Fun(commands.Cog):
             return
 
         if mem:
-            url = str(mem.avatar_url).replace('.webp', '.png')
+            url = str(mem.avatar_url).replace('webp', 'png')
 
-        response = requests.get(url)
-        _img = BytesIO(response.content)
-        _img.seek(0)
+        response = requests.get(f'{config.WEB_API_URL}/distort?img={url}')
+        if not response:
+            embed = embed_maker.message(ctx, 'Error getting image', colour='red')
+            return await ctx.send(embed=embed)
 
-        with wImage() as new_image:
-            with wImage(file=_img) as img:
-                if img.size >= (3000, 3000):
-                    embed = embed_maker.message(ctx, 'Image exceeds maximum resolution `3000x3000`', colour='red')
-                    return await ctx.send(embed=embed)
+        distorted_image = BytesIO(response.content)
+        distorted_image.seek(0)
 
-                def transform_image(img):
-                    img.transform(resize='800x800')
-                    for i in range(layers):
-                        img.liquid_rescale(width=int(img.width * 0.5), height=int(img.height * 0.5), delta_x=1)
-                        img.liquid_rescale(width=int(img.width * 1.5), height=int(img.height * 1.5), delta_x=2)
-
-                    img.transform(resize='800x800')
-
-                if len(img.sequence) > 1:
-                    transform_image(img.sequence[0])
-                    new_image.sequence.append(img.sequence[0])
-                else:
-                    transform_image(img)
-                    new_image.sequence.append(img)
-
-            magikd_buffer = BytesIO()
-            new_image.save(magikd_buffer)
-            magikd_buffer.seek(0)
-
-            embed = discord.Embed()
-            embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
-            embed.set_image(url='attachment://distorted.png')
-            return await ctx.send(file=discord.File(fp=magikd_buffer, filename='distorted.png'), embed=embed)
+        embed = discord.Embed()
+        embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
+        embed.set_image(url='attachment://distorted.png')
+        return await ctx.send(file=discord.File(fp=distorted_image, filename='distorted.png'), embed=embed)
 
 
 def setup(bot):
