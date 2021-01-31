222d74c20de050538b2dcdd6350aed6632745d36
hattyot <hattyot@gmail.com>
2021-01-31 14:03:46 +0200

    added files and support for putting bot in docker container

--- config_example.py
+++ config_example.py
@@ -1,9 +1,16 @@
+import os
+
 BOT_TOKEN = ''
 
 WEB_API_URL = ''
 # my web api github: https://github.com/Hattyot/image_processor
 
-MONGODB_URL = 'mongodb://localhost:27017'
+in_container = os.environ.get('IN_DOCKER', False)
+if in_container:
+    MONGODB_URL = 'mongodb://db:27017'
+else:
+    MONGODB_URL = 'mongodb://127.0.0.1:27017'
+
 PREFIX = '>>'
 EMBED_COLOUR = 0x00a6ad
 DEV_IDS = []
