fa7b6643e5a400f9815ba6073b01fa83e0c66785
hattyot <hattyot@gmail.com>
2021-02-10 23:02:00 +0200

    fixed sub_command help

--- cogs/utility.py
+++ cogs/utility.py
@@ -868,8 +868,8 @@ class Utility(commands.Cog):
                     return await embed_maker.message(ctx, f'{_cmd} is not a valid command')
 
                 if _sub_cmd:
-                    sub_cmd = cmd.get_command(_sub_cmd)
-                    if sub_cmd is None:
+                    cmd = cmd.get_command(_sub_cmd)
+                    if cmd is None:
                         return await embed_maker.message(ctx, f'{_sub_cmd} is not a valid sub command')
 
                 examples = f' | {prefix}'.join(cmd.examples)
