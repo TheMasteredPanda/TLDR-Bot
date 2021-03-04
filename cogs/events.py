799f2bbfac02221438df679b2fe61d3835f31472
hattyot <hattyot@gmail.com>
2021-03-04 15:53:56 +0200

    commands wont give points anymore

--- cogs/events.py
+++ cogs/events.py
@@ -44,7 +44,7 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.author.bot or not message.guild:
+        if message.author.bot or not message.guild or message.content.startswith(config.PREFIX):
             return
 
         leveling_cog = self.bot.get_cog('Leveling')
