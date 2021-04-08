dfb5303ee375f94425964aaba798d4879ef87711
hattyot <hattyot@gmail.com>
2021-04-08 16:02:51 +0300

    reaction_menus documentation

--- modules/reaction_menus.py
+++ modules/reaction_menus.py
@@ -12,17 +12,36 @@ from ttldict import TTLOrderedDict
 
 
 class ReactionMenus:
+    """
+    Class that manages reaction menus.
+
+    Attributes
+    __________
+    bot: :class:`bot.TLDR`
+        The discord bot.
+    menus: :class:`TTLOrderedDict`
+        Dict with TTL of five minutes
+    """
     def __init__(self, bot: bot.TLDR):
         self.bot = bot
-        self.bot.add_listener(self.on_reaction_add, 'on_reaction_add')
+        self.bot.add_listener(self._on_reaction_add, 'on_reaction_add')
 
         five_minutes = 5 * 60
         self.menus = TTLOrderedDict(default_ttl=five_minutes)
 
     def add(self, menu: Union[ReactionMenu, BookMenu]):
+        """
+        Adds menu to :attr:`menus`.
+
+        Parameters
+        ___________
+        menu: Union[:class:`ReactionMenu`, :class:`BookMenu`]
+            The menu that will be added to :attr:`menus`
+        """
         self.menus[menu.message.id] = menu
 
-    async def on_reaction_add(self, reaction: discord.Reaction, user: discord.User):
+    async def _on_reaction_add(self, reaction: discord.Reaction, user: discord.User):
+        """Event listener that handles reactions."""
         if user.bot:
             return
 
@@ -34,21 +53,34 @@ class ReactionMenus:
 
 
 class ReactionMenu:
+    """
+    Implements reaction menu.
+
+    Attributes
+    __________
+    message: :class:`discord.Message`
+        Message that acts as the menu.
+    buttons: :class:`dict`
+        All the buttons attached to the menu.
+    """
     def __init__(self, message: discord.Message, buttons: dict):
         self.message = message
         self.buttons = buttons
-        self.cooldown = time.time()
 
         for emote in buttons:
             asyncio.create_task(self.message.add_reaction(emote))
 
     async def call_function(self, reaction: discord.Reaction, user: discord.User):
-        # add a 2 second cooldown, so the buttons cant be spammed
-        if self.cooldown > time.time():
-            return
-        else:
-            self.cooldown = time.time() + 2
+        """
+        Calls a button function of the menu by it's reaction.
 
+        Parameters
+        ___________
+        reaction: :class:`discord.Reaction`
+            Reaction that will be used to call the function of a button.
+        user: :class:`discord.User`
+            User who pressed the button.
+        """
         # get emote in string format
         emote_str = reaction.emoji if type(reaction.emoji) == str else f'<:{reaction.emoji.name}:{reaction.emoji.id}>'
 
@@ -60,6 +92,22 @@ class ReactionMenu:
 
 
 class BookMenu(ReactionMenu):
+    """
+    Implements a special type of reaction menu with different pages.
+
+    Attributes
+    __________
+    buttons: :class:`list`
+        All the buttons attached to the menu.
+    author: :class:`discord.Member`
+        Member who created the menu.
+    page: :class:`int`
+        Current page of the BookMenu.
+    max_page_num: :class:`int`
+        The number of the last page of the BookMenu.
+    page_constructor: :func:
+        Function that creates the page embed.
+    """
     def __init__(
             self,
             message: discord.Message, *,
@@ -75,9 +123,9 @@ class BookMenu(ReactionMenu):
             ('➡️', self.page_forward),
         ]
 
+        # if extra_back or extra_forward are set, add extra buttons which moves the pages by n
         if extra_back:
             self.buttons.insert(0, ('⏪', functools.partial(self.page_back, pages_back=extra_back)))
-
         if extra_forward:
             self.buttons.append(('⏩', functools.partial(self.page_forward, pages_forward=extra_forward)))
 
@@ -91,6 +139,16 @@ class BookMenu(ReactionMenu):
         self.page_constructor = page_constructor
 
     async def page_back(self, _, user: discord.User, *, pages_back: int = 1):
+        """
+        Moves the BookMenu back by pages_back amount.
+
+        Parameters
+        ___________
+        user: :class:`discord.User`
+            User that pressed the button.
+        pages_back: :class:`int`
+            How many pages to go back by, defaults to 1
+        """
         if self.author.id != user.id:
             return
 
@@ -101,9 +159,19 @@ class BookMenu(ReactionMenu):
             self.page = self.max_page_num
 
         new_embed = await self.page_constructor(page=self.page)
-        return await self.message.edit(embed=new_embed)
+        await self.message.edit(embed=new_embed)
 
     async def page_forward(self, _, user: discord.User, *, pages_forward: int = 1):
+        """
+        Moves the BookMenu forward by pages_forward amount.
+
+        Parameters
+        ___________
+        user: :class:`discord.User`
+            User that pressed the button.
+        pages_forward: :class:`int`
+            How many pages to go forward by, defaults to 1
+        """
         if self.author.id != user.id:
             return
 
