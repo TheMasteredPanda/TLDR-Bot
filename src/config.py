1b484a5b6c33d3760c99eaf156bb59cf19a7f214
TheMasteredPanda <dukejakemorgan@gmail.com>
2021-11-12 20:33:12 +0000

    Undoing all of this. ffs me.

--- src/config.py
+++ src/config.py
@@ -23,5 +23,5 @@ SLACK_CLIENT_SECRET = config.get("SLACK_CLIENT_SECRET")
 SLACK_REDIRECT_DOMAIN = config.get("SLACK_REDIRECT_DOMAIN")
 EMBED_COLOUR = int(config.get("EMBED_COLOUR"), 16)
 
-MODULES = {"clearance": False, "google_drive": False, "leveling": False}
+MODULES = {}
 COGS = {}
